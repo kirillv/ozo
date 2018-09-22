9a49e39c81a6218c07dbbce8ca6e81b7501be005
Sergei L. Khandrikov <thed636@yandex-team.ru>
2018-09-22 13:34:08 +0300

    Update generated docs

---
+++ group__group-core-functions.js
@@ -0,0 +1,8 @@
+var group__group_core_functions =
+[
+    [ "allocate_nullable", "group__group-core-functions.html#gacdbea0d12bdc6a1279c010659190a1a3", null ],
+    [ "is_null", "group__group-core-functions.html#ga60f6f6924a9f600f1c2c6dcff2316d06", null ],
+    [ "is_null_recursive", "group__group-core-functions.html#ga602abbe1728381084559b85c3835b9bb", null ],
+    [ "unwrap", "group__group-core-functions.html#ga7d5539cb3d5d8e4caf46e62aa5fe27aa", null ],
+    [ "unwrap_recursive", "group__group-core-functions.html#ga76a7f893cb259d90cc91a3d7beb03a73", null ]
+];
